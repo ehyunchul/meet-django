0b0855f43d127cccc4f42b5ba49a0143386f6c53
HYUNCHUL <chul0830@gmail.com>
2018-04-10 19:15:12 +0900

    freeze

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hello(models.Model):
+    """만나면 반가워서 Hello"""
+    message = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.message
