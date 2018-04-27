1d11eb3c023956543e0ead912216bdf0742dee40
hyunchul-lee <chul0830@gmail.com>
2018-04-27 12:33:05 +0900

    add jupyter notebook

--- meet/settings.py
+++ meet/settings.py
@@ -31,14 +31,17 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
+
+    'django_extensions',
+
+    'hello.apps.HelloConfig',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
